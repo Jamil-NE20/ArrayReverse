4830606cd4161229bbf346b2c5c13e15e8f600bc
Md Jamil Uddin <azad1031@gmail.com>
2020-05-14 12:18:13 +0200

    Array Reverse Exercise

---
+++ ArrayReverse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input:\n");
+            int[] nums = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Enter your number {0} : ", i + 1);
+                nums[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("\n\nOutput:");
+            Console.WriteLine("\nResult of the reverse Array");
+
+            for (int i = 4; i >= 0; i--)
+                Console.WriteLine("{0}", nums[i]);
+            Console.WriteLine("\nEnter any key to quit!");
+            Console.ReadLine();
+        }
+    }
+}
